53c88e01b2164df22c505e3ea048398ab1d07480
Casey <arramine@mail.ru>
2023-07-11 11:23:00 +0300

    test

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ app.use(helmet());
 app.disable('x-powered-by');
 
 app.post('/signup',
-  // celebrate({
-  //   body: Joi.object().keys({
-  //     name: Joi.string().required().min(2).max(30),
-  //     about: Joi.string().required().min(2).max(30),
-  //     avatar: Joi.string().required(),
-  //     email: Joi.string().required().email(),
-  //     password: Joi.string().required(),
-  //   }),
-  // }),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
   createUser
 );
 app.post('/signin', login);
